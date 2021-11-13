5d15982de15082de9ba7bceee201c03521dcd5b1
Luka Heddens <lukaheddens@gmail.com>
2021-11-13 04:58:01 +0100

    removed default config values and edit readme

--- config/rocket.php
+++ config/rocket.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'url' => env('ROCKET_URL', 'localhost:3000'),
-    'authToken' => env('ROCKET_AUTH_TOKEN', 'rocket_auth_token'),
+    'url' => env('ROCKET_URL'),
+    'authToken' => env('ROCKET_AUTH_TOKEN'),
     'user' => [
-        'id' => env('ROCKET_USER_ID', 'rocket_user_id'),
-        'name' => env('ROCKET_USER_NAME', 'rocket_user_name'),
-        'password' => env('ROCKET_USER_PASSWORD', 'rocket_user_password'),
+        'id' => env('ROCKET_USER_ID'),
+        'name' => env('ROCKET_USER_NAME'),
+        'password' => env('ROCKET_USER_PASSWORD'),
     ],
 ];
