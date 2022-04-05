72e1488cf56460f8a5783c876ff539002ecf9b45
Luka Heddens <luka@visifo.com>
2022-04-05 10:43:40 +0200

    added cleanHistory

---
+++ src/Endpoints/Room.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace visifo\Rocket\Endpoints;
+
+use visifo\Rocket\Rocket;
+use visifo\Rocket\RocketException;
+
+class Room extends Endpoint
+{
+    private Rocket $rocket;
+
+    public function __construct(Rocket $rocket)
+    {
+        $this->rocket = $rocket;
+    }
+
+    /**
+     * @throws RocketException
+     */
+    public function cleanHistory(string $roomId, string $latest, string $oldest): void
+    {
+        $this->checkEmptyString($roomId)
+            ->checkEmptyString($latest)
+            ->checkEmptyString($oldest);
+
+        $data = get_defined_vars();
+        $this->rocket->post('rooms.cleanHistory', $data);
+    }
+
+}
